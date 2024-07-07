f8ed7fce3b0176d92ea012455e72795f1c5660e5
zeeshan-Ahmed21 <zeeshanah4321@gmail.com>
2024-07-07 11:42:05 +0500

    'Username

---
+++ script.js
@@ -0,0 +1,9 @@
+let username = prompt("Please Enter Your Username");
+let pass = prompt("Please Enter Your Password");
+
+if(username=="Bilal" && pass =="bmj"){
+    alert("Successfully Login");
+}
+else{
+    alert("Invalid Username or password");
+}
